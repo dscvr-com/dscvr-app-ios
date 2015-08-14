f1747c88d10ae52878b8cbe8a346b034dec54487
emiswelt <emiswelt@gmail.com>
2015-08-14 18:59:52 +0200

    Updated ios-wrapper

--- Optonaut/Utils/Stitcher.h
+++ Optonaut/Utils/Stitcher.h
@@ -1,7 +1,30 @@
 #import <Foundation/Foundation.h>
+#import <GLKit/GLKit.h>
 
-@interface Stitcher : NSObject
+@interface ImageBuffer : NSObject
+@property void* data;
+@property int width;
+@property int height;
+@end
 
-+ (bool)push:(double [])extrinsics :(double [])intrinsics :(void *)image :(int)width :(int)height :(double [])newExtrinsics :(int)frameCount;
+@interface SelectionPoint : NSObject
+@property GLKMatrix4 extrinsics;
+@property int id;
+@property int ringId;
+@property int localId;
+@end
+
+@interface IosPipeline : NSObject
+
+//This interface takes matrices in SCNSpace and also gives them back in SCNSpace.
+//It's that simple.
+
++ (void)Push:(GLKMatrix4)extrinsics :(GLKMatrix3)intrinsics :(ImageBuffer*)image;
++ (GLKMatrix4)GetCurrentRotation;
++ (bool)IsPreviewImageValialble;
++ (ImageBuffer*)GetPreviewImage;
++ (void)FreeImageBuffer:(ImageBuffer*)toFree;
++ (NSArray<SelectionPoint*>*)GetSelectionPoints;
++ (void)DisableSelectionPoint:(SelectionPoint*)toDisable;
 
 @end
