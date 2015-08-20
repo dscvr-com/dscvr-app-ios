9e4bb523bac0ffb8fb24259e25b7452481c5329b
Johannes Schickling <schickling.j@gmail.com>
2015-08-20 12:02:25 +0200

    update cartfile

--- Optonaut/Views/Camera/CreateOptographViewController.swift
+++ Optonaut/Views/Camera/CreateOptographViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import ReactiveCocoa
+import KMPlaceholderTextView
 
 class CreateOptographViewController: UIViewController, RedNavbar {
     
--- Optonaut/Views/Details/NewCommentTableViewCell.swift
+++ Optonaut/Views/Details/NewCommentTableViewCell.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ReactiveCocoa
+import KMPlaceholderTextView
 
 class NewCommentTableViewCell: UITableViewCell {
     
