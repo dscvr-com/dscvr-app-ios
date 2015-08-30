8bb4a594a524b6c76c05243f5805ce1c1b66da79
emiswelt <emiswelt@gmail.com>
2015-08-30 11:15:56 +0200

    Saved a little on memory

--- Optonaut/Utils/Stitcher/Stitcher.mm
+++ Optonaut/Utils/Stitcher/Stitcher.mm
@@ -43,16 +43,23 @@ optonaut::ImageRef ImageBufferToImageRef(ImageBuffer image) {
 }
 
 ImageBuffer CVMatToImageBuffer(const cv::Mat &input) {
-    Mat converted(input.rows, input.cols, CV_8UC4);
-    cv::cvtColor(input, converted, COLOR_RGB2RGBA);
-    
     ImageBuffer output;
     output.width = input.cols;
     output.height = input.rows;
     output.data = malloc(output.width * output.height * 4);
-    memcpy(output.data, converted.data, output.width * output.height * 4);
+    
+    Mat converted(input.rows, input.cols, CV_8UC4, output.data);
+    cv::cvtColor(input, converted, COLOR_RGB2RGBA);
     
     return output;
+}
+
+CGImageRef CVMatToCGImage(const cv::Mat &input) {
+    CGContextRef ctx = CGBitmapContextCreate(input.data, input.cols, input.rows, 8, input.cols * 3, CGColorSpaceCreateDeviceRGB(), kCGBitmapByteOrder32Big | kCGImageAlphaNone);
+    
+    assert(ctx != nullptr);
+    
+    return CGBitmapContextCreateImage(ctx);
 }
 
 SelectionPoint ConvertSelectionPoint(optonaut::SelectionPoint point) {
