e411ee6bab29ae968a81a2744876dd025387d568
Johannes Schickling <schickling.j@gmail.com>
2015-10-06 22:32:58 +0100

    Removed permissionscope

--- Optonaut/Views/Camera/CreateOptographViewController.swift
+++ Optonaut/Views/Camera/CreateOptographViewController.swift
@@ -11,7 +11,6 @@ import ReactiveCocoa
 import KMPlaceholderTextView
 import Mixpanel
 import ActiveLabel
-import PermissionScope
 import AVFoundation
 
 class CreateOptographViewController: UIViewController, RedNavbar {
@@ -38,44 +37,8 @@ class CreateOptographViewController: UIViewController, RedNavbar {
     let hashtagInputView = KMPlaceholderTextView()
     let lineView = UIView()
     
-    var stitcherDisposable: Disposable?
-    
-    let assetSignalProducer: SignalProducer<OptographAsset, NoError>
-    
-    required init(assetSignalProducer: SignalProducer<OptographAsset, NoError>) {
-        self.assetSignalProducer = assetSignalProducer
-        
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let pscope = PermissionScope()
-        
-        pscope.headerLabel.text = "Where are you?"
-        pscope.bodyLabel.text = "Please share your location\r\nto tag your Optograph"
-        pscope.unauthorizedButtonColor = UIColor.Accent
-        pscope.permissionButtonTextColor = UIColor.Accent
-        pscope.permissionButtonBorderColor = UIColor.Accent
-        pscope.permissionLabelColor = UIColor(0x4d4d4d)
-        pscope.closeButton.titleLabel?.font = UIFont.icomoonOfSize(30)
-        pscope.closeButton.setTitle(String.icomoonWithName(.Cross), forState: .Normal)
-        pscope.closeButtonTextColor = UIColor(0x4d4d4d)
-        pscope.closeOffset = CGSize(width: 7, height: 5)
-        
-        pscope.addPermission(LocationWhileInUsePermission(), message: "This step is just needed once")
-        
-        pscope.show(
-            { finished, results in },
-            cancelled: { _ in
-                self.navigationController?.popViewControllerAnimated(false)
-            }
-        )
         
         view.backgroundColor = .whiteColor()
         
@@ -166,19 +129,6 @@ class CreateOptographViewController: UIViewController, RedNavbar {
         setupCamera()
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "dismissKeyboard"))
-        
-        stitcherDisposable = assetSignalProducer
-            .startOn(QueueScheduler(queue: dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)))
-            .on(next: { asset in
-                self.viewModel.saveAsset(asset)
-            })
-            .observeOn(UIScheduler())
-            .on(completed: {
-                self.viewModel.pending.value = false
-            })
-            .start()
-        
-        // TODO make signalproducer to optional -> set to nil
         
         view.setNeedsUpdateConstraints()
     }
@@ -299,7 +249,7 @@ class CreateOptographViewController: UIViewController, RedNavbar {
     func cancel() {
         Mixpanel.sharedInstance().track("Action.CreateOptograph.Cancel")
         
-        stitcherDisposable?.dispose()
+        StitchingService.startStitching()
         navigationController?.popViewControllerAnimated(false)
     }
     
