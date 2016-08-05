39fd3354d14d99f3079d4d0fd8261153916584fb
robert <robert.alkuino@gmail.com>
2016-08-05 20:10:53 +0800

    comment the stitchingservice saving er

--- Optonaut/Views/TabViewController.swift
+++ Optonaut/Views/TabViewController.swift
@@ -403,7 +403,7 @@ class TabViewController: UIViewController,UIGestureRecognizerDelegate,UIScrollVi
         dividerThree.hidden = true
         
         let versionLabel = UILabel()
-        versionLabel.text = "v0.90"
+        versionLabel.text = "v0.91"
         versionLabel.textAlignment = .Center
         versionLabel.font = .fontDisplay(10, withType: .Semibold)
         versionLabel.align(.UnderMatchingRight, relativeTo: bgImage!, padding: 2, width: 40, height: 10)
