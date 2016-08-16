38ecb338c932909183883646a60efe65cf3be577
mcandres <mcandres888@gmail.com>
2016-08-16 17:36:07 +0800

    workin metadata

--- Optonaut/Stitcher/Stitcher.mm
+++ Optonaut/Stitcher/Stitcher.mm
@@ -82,11 +82,11 @@ struct StitcherCancellation {
     
     optonaut::Stitcher stitcher(Stores::left);
     cv::Mat sphere = stitcher.Finish(optonaut::ProgressCallback::Empty)->image.data;
-    cv::Mat blurred;
-    optonaut::PanoramaBlur panoBlur(sphere.size(), cv::Size(sphere.cols, std::max(sphere.cols / 2, sphere.rows)));
-    panoBlur.Blur(sphere, blurred);
+    //cv::Mat blurred;
+    //optonaut::PanoramaBlur panoBlur(sphere.size(), cv::Size(sphere.cols, std::max(sphere.cols / 2, sphere.rows)));
+    //panoBlur.Blur(sphere, blurred);
 
-    return CVMatToImageBuffer(blurred);
+    return CVMatToImageBuffer(sphere);
     
 }
 
