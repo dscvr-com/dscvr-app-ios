158bf57c4572e7ca54beb4657fec37b6660ade34
robert <robert.alkuino@gmail.com>
2016-11-15 18:00:21 +0800

    merged motor on current apps

---
+++ Optonaut/BService.swift
@@ -0,0 +1,13 @@
+//
+//  BService.swift
+//  DSCVR
+//
+//  Created by robert john alkuino on 11/15/16.
+//  Copyright © 2016 Optonaut. All rights reserved.
+//
+
+import UIKit
+
+class BService: NSObject {
+
+}
---
+++ Optonaut/BTList.swift
@@ -0,0 +1,13 @@
+//
+//  BTList.swift
+//  DSCVR
+//
+//  Created by robert john alkuino on 11/15/16.
+//  Copyright © 2016 Optonaut. All rights reserved.
+//
+
+import UIKit
+
+class BTList: NSObject {
+
+}
