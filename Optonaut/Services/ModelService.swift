66e68d02242014c41d98d83e4b693211a2e5f751
Philipp Meyer <philipp-meyer-freital@gmx.de>
2017-05-18 01:52:43 +0200

    even more than before clean up :recycle::recycle::recycle:

--- Optonaut/Services/ModelService.swift
+++ Optonaut/Services/ModelService.swift
@@ -88,10 +88,7 @@ extension ModelBox where M: SQLiteModel {
 }
 
 class Models {
-    
     static var optographs = ModelCache<Optograph>()
-//    static var persons = ModelCache<Person>()
-//    static var locations = ModelCache<Location>()
 }
 
 protocol ModelCacheType: class {
